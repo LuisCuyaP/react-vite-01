7e8c9e3e375ca411d2fe3150313fc921714b1eaa
Luis Cuya <luis.cuya@intelica.com>
2025-09-04 09:35:41 -0500

    feat: creacion de mi primer componente en react

---
+++ first-steps/src/FirstStepApp.tsx
@@ -0,0 +1,14 @@
+export function FirstStepApp() {
+    return (
+        <>
+            <h1>My React App</h1>
+            <p>Esto es un parrafo</p>
+            
+            <button>click me</button>
+
+            <div>
+                <h2>Esto es un subtitulo</h2>
+            </div>
+        </>
+    )
+}
