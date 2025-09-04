7e41fe3ab55b8f11963c7f6813d38def0792cbfb
Luis Cuya <luis.cuya@intelica.com>
2025-09-04 09:56:20 -0500

    feat: se agrega estilos css a un componente

--- first-steps/src/MiNombreApp.tsx
+++ first-steps/src/MiNombreApp.tsx
@@ -1,3 +1,4 @@
+import type { CSSProperties } from "react";
 
 const firstName = 'Luis'
 const lastName = 'Cuya'
@@ -8,6 +9,13 @@ const address = {
     city: 'Anytown',
     country: 'USA'
 };
+const myStyles: CSSProperties = {
+    whiteSpace: 'pre-wrap',
+    backgroundColor: 'black',
+    color: 'white',
+    padding: '10px',
+    borderRadius: isActive ? '10px' : '0px'
+}
 
 export const MiNombreApp = () => {
     return (
@@ -16,7 +24,12 @@ export const MiNombreApp = () => {
             <h3>Mi Apellido es {lastName}</h3>
             <p>{favoriteGames.join(', ')}</p>
             <h1>{isActive ? 'Activo' : 'Inactivo'}</h1>
-            <h1>{JSON.stringify(address)}</h1>
+           
+            <p 
+             style={myStyles}
+            >
+                <h1>{JSON.stringify(address)}</h1>
+            </p>
         </>
     );
 };
